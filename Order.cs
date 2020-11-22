a4160ad521f983ba6f4c43a714b171f962986825
Bianca <itu.bianca11@yahoo.com>
2020-11-22 08:57:11 -0800

    Add project files.

---
+++ Order.cs
@@ -0,0 +1,18 @@
+namespace Itu_AnaBianca_Laborator6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Order")]
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+
+        public int? CustId { get; set; }
+
+        public int? CarId { get; set; }
+    }
+}
---
+++ StringMinLengthValid.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Itu_AnaBianca_Laborator6
+{
+    internal class StringMinLengthValid : ValidationRule
+    {
+        public StringMinLengthValid()
+        {
+        }
+    }
+}
